fd6c6b4ce9ecf163a956206ccf500a7a4a9d8b2b
Matthew Pan <matthew.pan@queensu.ca>
2026-01-12 23:37:46 -0500

    more debugging

--- ipc/udp.py
+++ ipc/udp.py
@@ -8,6 +8,10 @@ class UdpDetectionSender:
     def __init__(self, addr=DEFAULT_ADDR):
         self.addr = addr
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # Set socket to non-blocking to prevent buffering delays
+        self.sock.setblocking(False)
+        # Reduce send buffer to prevent kernel buffering
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 65536)
 
     def send(self, objects, frame_id):
         msg = {
@@ -15,4 +19,8 @@ class UdpDetectionSender:
             "frame_id": frame_id,
             "objects": objects,
         }
-        self.sock.sendto(json.dumps(msg).encode("utf-8"), self.addr)
+        try:
+            self.sock.sendto(json.dumps(msg).encode("utf-8"), self.addr)
+        except BlockingIOError:
+            # Socket buffer full, drop this packet (expected for real-time streaming)
+            pass
